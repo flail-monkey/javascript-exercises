42969fc3f0993d5e0eb13d067bc3a3d5bbba6c38
Joal <flail-monkey@users.noreply.github.com>
2025-04-08 19:10:27 -0400

    04_removeFromArray - all tests pass

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
 const removeFromArray = function() {
+    args = Array.from(arguments)
+    targetArray = args[0]
+    removeArgs = args.slice(1)
+
+    let resultArray = []
+    for (item of targetArray)
+        {
+        if (!(removeArgs.includes(item)))
+            {resultArray.push(item)}
+        }
+
+    return resultArray
 };
 
 // Do not edit below this line
