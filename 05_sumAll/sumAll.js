8111eb3e871454a8871ceb51004cd8d5d4217055
Joal <flail-monkey@users.noreply.github.com>
2025-04-08 20:27:22 -0400

    05_sumAll - all tests pass

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    // no negative numbers
+    if (start < 0 || end < 0)
+        {return "ERROR"}
+    // must be an integer
+    if (!(Number.isInteger(start)) || !(Number.isInteger(end)))
+        {return "ERROR"}
+    // reorder if first number is bigger
+    if (start > end)
+        {
+        e = start
+        s = end
+        }
+    else
+        {
+        s = start
+        e = end
+        }
 
+    let total = 0
+    for (let i = s; i <= e; i++)
+        {total += i}
+    return total
 };
 
 // Do not edit below this line
