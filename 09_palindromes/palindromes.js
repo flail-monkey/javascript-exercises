efb7dfb912f6a6772b9b2e3f028cb78d83cbdcfb
Joal <flail-monkey@users.noreply.github.com>
2025-05-06 13:30:10 -0400

    palindromes works. all tests pass.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    lowerWord = word.toLowerCase()
+    validWord = lowerWord.replace(/[^\w\d]/g,"")
+    stringArray = validWord.split("")
+    reversed = stringArray.reverse().join("")
+    if (validWord === reversed)
+        {
+        return true
+        }
+    else {return false}
 };
 
 // Do not edit below this line
