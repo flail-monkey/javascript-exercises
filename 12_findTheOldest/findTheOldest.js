274eda2018386a0a5033655c1b818cd67950198d
Joal <flail-monkey@users.noreply.github.com>
2025-05-06 15:56:46 -0400

    findTheOldest complete. all tests pass.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    oldestAge = 0
+    oldestPerson = null
+    const currentYear = 2025
+
+    for (i=0; i<people.length; i++)
+        {
+        // get person info
+        person = people[i]
+        if (person.yearOfDeath === undefined)
+            {yearOfDeath = currentYear}
+        else {yearOfDeath = person.yearOfDeath}
+
+        // calculate age
+        age = yearOfDeath - person.yearOfBirth
+
+        // update if oldest
+        if (age > oldestAge)
+            {
+            oldestAge = age
+            oldestPerson = person
+            }
+        }
+    return oldestPerson
 
 };
 
