4cf5018742aee59cf9f2a6ada80e1fac1dd91b63
Joal <flail-monkey@users.noreply.github.com>
2025-09-16 15:16:40 -0400

    updated getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,11 +1,26 @@
-const getTheTitles = function(books) {
-    titles = []
-    for (i=0; i<books.length; i++)
-        {
-        titles.push(books[i].title)
-        }
-    return titles
-};
+// const getTheTitles = function(books) {
+//     titles = []
+//     for (i=0; i<books.length; i++)
+//         {
+//         titles.push(books[i].title)
+//         }
+//     return titles
+// };
+
+// function getTheTitles(books)
+//     {
+//     titles = []
+//     for (i=0; i<books.length; i++)
+//         {
+//         titles.push(books[i].title)
+//         }
+//     return titles
+//     }
+
+function getTheTitles(books)
+    {
+    return books.map(item => item.title)
+    }
 
 // Do not edit below this line
 module.exports = getTheTitles;
