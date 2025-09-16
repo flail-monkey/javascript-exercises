52e71bb5cf0909edb788fc29d20e0ffa0874705d
Joal <flail-monkey@users.noreply.github.com>
2025-09-16 16:05:53 -0400

    updated findTheOldest with reduce method

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,29 +1,46 @@
-const findTheOldest = function(people) {
-    oldestAge = 0
-    oldestPerson = null
-    const currentYear = 2025
+// const findTheOldest = function(people) {
+//     oldestAge = 0
+//     oldestPerson = null
+//     const currentYear = 2025
 
-    for (i=0; i<people.length; i++)
-        {
-        // get person info
-        person = people[i]
-        if (person.yearOfDeath === undefined)
-            {yearOfDeath = currentYear}
-        else {yearOfDeath = person.yearOfDeath}
+//     for (i=0; i<people.length; i++)
+//         {
+//         // get person info
+//         person = people[i]
+//         if (person.yearOfDeath === undefined)
+//             {yearOfDeath = currentYear}
+//         else {yearOfDeath = person.yearOfDeath}
 
-        // calculate age
-        age = yearOfDeath - person.yearOfBirth
+//         // calculate age
+//         age = yearOfDeath - person.yearOfBirth
 
-        // update if oldest
-        if (age > oldestAge)
-            {
-            oldestAge = age
-            oldestPerson = person
-            }
-        }
-    return oldestPerson
+//         // update if oldest
+//         if (age > oldestAge)
+//             {
+//             oldestAge = age
+//             oldestPerson = person
+//             }
+//         }
+//     return oldestPerson
 
-};
+// };
+
+function calcAge(birth, death)
+    {
+    if (!death) {death = new Date().getFullYear()}
+    return death - birth
+    }
+
+function findTheOldest(people)
+    {
+    return people.reduce((oldest, current) =>
+                         {
+                         oldestAge = calcAge(oldest.yearOfBirth, oldest.yearOfDeath);
+                         currentAge = calcAge(current.yearOfBirth, current.yearOfDeath);
+                         return oldestAge > currentAge ? oldest : current;
+                         }
+                        )
+    }
 
 // Do not edit below this line
 module.exports = findTheOldest;
